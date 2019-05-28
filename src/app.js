5fd84bda0881a02f4371f00a949ac635cf2aa1db
singhtalvinder <talvindersingh.ca@gmail.com>
2019-05-28 13:41:22 -0400

    some more hbs and partials sample but useful stuff

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs') // required for partials.
 
+// NOTE:  nodemon doesn't detect changes to the hbs files so we need to tell
+// nodemon to take care of that as well as below:
+// cmd>nodemon .\src\app.js -e js,hbs
+
+
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
@@ -27,12 +32,11 @@ hbs.registerPartials(partialPath)
 // static directory to serve.
 app.use(express.static(publicDirPath))
 
-
 // Now to use this, we specify it as below. Parameters can be passed as second parameter.
 app.get('',(req, res) => {
     res.render('index', {
         title: 'Weather app',
-        name: 'Developer'
+        name: 'TSingh'
     })
 })
 
@@ -46,8 +50,9 @@ app.get('/about', (req, res) =>{
 
 app.get('/help', (req, res) =>{
     res.render('help', {
+        helpText: "Help Documentation.",
         title: 'Find all the help here .',
-        name: 'contents in progress...'
+        name: 'TSingh'
     })
 
 })
@@ -69,7 +74,24 @@ app.get('/help', (req, res) =>{
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'cloudy',
-        location: 'Burlin, on'
+        location: 'Toronto, on'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'TSingh',
+        errorMessage: 'Help article not found !!'
+    })
+
+})
+// not found url goes here.
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'TSingh',
+        errorMessage: 'Page not found !!'
     })
 })
 
